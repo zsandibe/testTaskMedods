8c7eb2cff29a4c0994ce5fe7ff044780ac8cb78f
zsandibe <Sandibekov1998@gmail.com>
2024-02-08 00:53:00 +0600

    upd:added config to service field

--- config/config.go
+++ config/config.go
@@ -9,13 +9,15 @@ import (
 )
 
 type Config struct {
-	MongoUrl       string
-	NameDb         string
-	NameCollection string
-	AccessKey      string
-	RefreshKey     string
-	ServerHost     string
-	ServerPort     string
+	MongoUrl        string
+	NameDb          string
+	NameCollection  string
+	AccessKey       string
+	RefreshKey      string
+	ServerHost      string
+	ServerPort      string
+	AccessTokenAge  string
+	RefreshTokenAge string
 }
 
 func NewConfig() Config {
@@ -25,11 +27,13 @@ func NewConfig() Config {
 	}
 
 	return Config{
-		MongoUrl:       os.Getenv("MONGODB_URL"),
-		NameDb:         os.Getenv("NAME_DB"),
-		NameCollection: os.Getenv("NAME_COLLECTION"),
-		AccessKey:      os.Getenv("ACCESS_KEY"),
-		ServerHost:     os.Getenv("SERVER_HOST"),
-		ServerPort:     os.Getenv("SERVER_PORT"),
+		MongoUrl:        os.Getenv("MONGODB_URL"),
+		NameDb:          os.Getenv("NAME_DB"),
+		NameCollection:  os.Getenv("NAME_COLLECTION"),
+		AccessKey:       os.Getenv("ACCESS_KEY"),
+		ServerHost:      os.Getenv("SERVER_HOST"),
+		ServerPort:      os.Getenv("SERVER_PORT"),
+		AccessTokenAge:  os.Getenv("ACCESS_TOKEN_AGE"),
+		RefreshTokenAge: os.Getenv("REFRESH_TOKEN_AGE"),
 	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"testTaskMedods/config"
 	"testTaskMedods/internal/domain"
 	"testTaskMedods/internal/repository"
 )
@@ -11,11 +12,13 @@ type Service interface {
 }
 
 type service struct {
+	conf config.Config
 	repo repository.Repository
 }
 
-func NewService(repo repository.Repository) *service {
+func NewService(repo repository.Repository, conf config.Config) *service {
 	return &service{
 		repo: repo,
+		conf: conf,
 	}
 }
